8a1b9100ed7ab0492fe5bbe6775ffcd27fed5c55
Oboni ANower <oboni_anower@yahoo.com>
2023-01-08 19:01:39 +0000

    t22.2

---
+++ hash.py
@@ -0,0 +1,7 @@
+countryMap = {'England': 'London',
+              'Sweden': 'Stockholm',
+              'Japan': 'Tokyo',
+              'Malaysia': 'Kuala Lumpur'}
+
+print((countryMap['Sweden']))  # returns Stockholm
+# Key is used to search the value
